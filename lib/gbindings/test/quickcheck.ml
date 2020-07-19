f4fd0186fc712ad54966878097747f88543d4fbb
Bob Poekert <bob@poekert.com>
2020-07-19 12:52:46 -0400

    tests still fail but I think the bugs are in the tests at this point

--- lib/gbindings/test/quickcheck.ml
+++ lib/gbindings/test/quickcheck.ml
@@ -1,26 +1,13 @@
 
-module RS = Random.State
-
 let test_roundtrip typ typ_name encoder decoder = 
     QCheck.Test.make ~count:10000
         ~name:(Printf.sprintf "%s_roundtrip" typ_name)
         typ (fun v -> (decoder (encoder v)) == v)
 
-let rec n_bits_mask off res =
-    if off >= 0 then
-        n_bits_mask
-            (off - 1)
-            (res land (1 lsl off))
-    else res
-
-let gen_bits n_bits = 
-    let mask = n_bits_mask n_bits 0 in 
-    (fun st -> (RS.bits st) land mask)
-
-let signed_gen_bits n_bits = 
-    let gb = gen_bits n_bits in 
-    (fun st -> (gb st) * (if RS.bool st then 1 else -1))
-
+let test_roundtrip_float typ typ_name encoder decoder = 
+    QCheck.Test.make ~count:10000
+        ~name:(Printf.sprintf "%s_roundtrip" typ_name)
+        typ (fun v -> (abs_float (decoder (encoder v)) -. v) < 0.5)
 
 let roundtrip_tests = 
     let open QCheck in 
@@ -30,17 +17,17 @@ let roundtrip_tests =
         test_roundtrip bool "boolean" g_value_of_boolean boolean_of_g_value;
         test_roundtrip (-127 -- 127) "char" g_value_of_char char_of_g_value;
         test_roundtrip (int_bound 255) "uchar" g_value_of_uchar uchar_of_g_value;
+        test_roundtrip_float float "float" g_value_of_float float_of_g_value;
+        test_roundtrip_float float "double" g_value_of_double double_of_g_value;
+        QCheck.Test.make ~count:10000
+            ~name:"string_roundtrip"
+            string (fun v -> String.equal (string_of_g_value (g_value_of_string v)) v);
         test_roundtrip int "int" g_value_of_int int_of_g_value;
         test_roundtrip int "uint" g_value_of_uint uint_of_g_value;
         test_roundtrip int64 "long" g_value_of_long long_of_g_value;
         test_roundtrip ui64 "ulong" g_value_of_ulong ulong_of_g_value;
         test_roundtrip int64 "int64" g_value_of_int64 int64_of_g_value;
         test_roundtrip ui64 "uint64" g_value_of_uint64 uint64_of_g_value;
-        test_roundtrip float "float" g_value_of_float float_of_g_value;
-        test_roundtrip float "double" g_value_of_double double_of_g_value;
-        QCheck.Test.make ~count:10000
-            ~name:"string_roundtrip"
-            string (fun v -> String.equal (string_of_g_value (g_value_of_string v)) v)
     ]
 
 (*
